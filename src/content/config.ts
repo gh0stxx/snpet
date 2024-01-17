f973008ee8f2d78d820a24a7577b85aba1fbc4fc
gh0stxx <gh0st0x9@gmail.com>
2024-01-18 10:42:40 +1100

    fix formatting

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,16 @@
 import { defineCollection, z } from "astro:content";
 const blogCollection = defineCollection({
-    type: "content",
-    schema: ({image}) => z.object({
-        title: z.string(),
-        pubDate: z.date(),
-        author: z.string(), 
-        image: image(),
-        tags: z.array(z.string())
-    })
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      author: z.string(),
+      image: image(),
+      tags: z.array(z.string()),
+    }),
 });
 
 export const collections = {
-    blog: blogCollection,
+  blog: blogCollection,
 };
