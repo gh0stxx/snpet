316df2bbcd51e12daf509d867ca54eb3bce8b899
gh0stxx <gh0st0x9@gmail.com>
2024-01-22 11:26:47 +1100

    font test

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 	'./node_modules/flowbite/**/*.js'],
 	theme: {
 		fontFamily: {
-			sans: ['Poppins', 'sans-serif'],
+			sans: ['Poppins', 'sans-serif', ...defaultTheme.fontFamily.sans],
 		},
 		extend: {
 			objectFit: ['cover', 'contain', 'fill', 'none', 'scale-down'],
